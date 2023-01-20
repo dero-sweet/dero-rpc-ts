60b8ae6e310788335391245c0de8d12ee64c5032
arch <info@littlelakestudios.com>
2023-01-19 23:36:01 -0800

    feat: better names

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-export * as DeroWasmWalletMethods from './DeroWasmWalletMethods';
-export * as DeroWasmWalletTypes from './DeroWasmWalletTypes';
+// types for query and response
 export * as DeroRpcWalletTypes from './DeroRpcWalletTypes';
 export * as DeroRpcTypes from './DeroRpcTypes';
-export * as DeroRpcBridgeWalletMethods from './DeroRpcBridgeWalletMethods';
+
+// wasm
+export * as DeroWasmWalletMethods from './DeroWasmWalletMethods';
+export * as DeroWasmWalletTypes from './DeroWasmWalletTypes';
+
+// rpc bridge api
 export * as DeroRpcBridgeMethods from './DeroRpcBridgeMethods';
+export * as DeroRpcBridgeWalletMethods from './DeroRpcBridgeWalletMethods';
+
+// rpcs
+export * as DeroRpcMethods from './DeroRpcMethods';
+export * as DeroRpcWalletMethods from './DeroRpcWalletMethods';
+
+// rpc wrapper
 export * as DeroRpc from './DeroRpc';
