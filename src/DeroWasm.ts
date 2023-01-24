f2d69762dc5b9fe89dd0e67561bad1c7f340d7b4
arch <info@littlelakestudios.com>
2023-01-23 22:22:53 -0800

    feat: clean up wasm methods

--- src/DeroWasm.ts
+++ src/DeroWasm.ts
@@ -1,4 +1,4 @@
-import { DeroWasmWindow } from './DeroWasmTypes';
+import { DeroWasmWalletResponse, DeroWasmWindow } from './DeroWasmTypes';
 
 //
 // Wallet
@@ -9,40 +9,50 @@ export interface DeroCreateNewWalletParams {
 }
 
 export interface DeroCreateNewWalletResult {
-  //{
-  //     "hexSeed": "2d1f79dc85f304f5e704d88abca4b9423af9fb47b4ebd2d416ba4324914f0340",
-  //     "fileData": {},
-  //     "address": "dero1qy3mphzg2fkscrgv74uq4ct9ez4g0cteyn6dxp0jtju0eup9cn6dwqggnuuly",
-  //     "seed": "tepid unknown scuba toffee powder oxygen duplex faxed swiftly highway sober apex evaluate apricot gimmick kisses wonders stacking adept cactus fall evenings lamb ripped stacking"
-  // }
   hexSeed: string;
   fileData: ArrayBuffer; //Uint8Array
   address: string;
   seed: string;
+  json?: any;
 }
 
 export const CreateNewWallet = async (params: DeroCreateNewWalletParams) => {
   const res = DeroWasmWindow.CreateNewWallet(params.password);
   if (res.value) {
-    console.log('there is value');
+    const value: DeroCreateNewWalletResult = {
+      ...res.value,
+      json: convertFileDataToJson(res.value.fileData),
+    };
+    res.value = value;
   }
-  const value: DeroCreateNewWalletResult = res.value;
-  res.value = value;
   return res;
 };
 
-export const FastRegister = async () => {
-  const asyncKey = 'FastRegister';
-  const res = DeroWasmWindow.FastRegister(asyncKey);
-
+export const CloseWallet = async (walletKey: string) => {
+  const res = DeroWasmWindow.CloseWallet(walletKey);
   return res;
 };
-/*
-let fastRegResp = window.FastRegister(1)
-console.log("fastRegResp",fastRegResp)
-let key = `RegistrationStatus_<number: 1>`
-console.log("window",window[key])
-*/
+
+export const CloseAllWallets = async () => {
+  const res = DeroWasmWindow.CloseAllWallets();
+  return res;
+};
+
+export interface DeroWasmOpenWalletI {
+  walletKey: string;
+  password: string;
+  fileData: any; // Uint8Array
+}
+
+export const DeroWasmOpenWallet = async (params: DeroWasmOpenWalletI) => {
+  const res = DeroWasmWindow.OpenWallet(
+    params.walletKey,
+    params.password,
+    params.fileData,
+    true
+  );
+  return res;
+};
 
 export interface DeroRecoverWalletFromHexSeedParams {
   password: string;
@@ -58,10 +68,13 @@ export const DeroRecoverWalletFromHexSeed = async (
   );
   console.log(res);
   if (res.value) {
-    console.log('there is value');
+    const value: DeroCreateNewWalletResult = {
+      ...res.value,
+      json: convertFileDataToJson(res.value.fileData),
+    };
+    res.value = value;
   }
-  const value: DeroCreateNewWalletResult = res.value;
-  return value;
+  return res;
 };
 
 export interface DeroRecoverWalletFromSeedParams {
@@ -72,16 +85,68 @@ export interface DeroRecoverWalletFromSeedParams {
 export const DeroRecoverWalletFromSeed = async (
   params: DeroRecoverWalletFromSeedParams
 ) => {
-  //@ts-ignore
   const res = DeroWasmWindow.RecoverWalletFromSeed(
     params.password,
     params.seed
   );
   console.log(res);
-  if (!res.value) return;
-  const value: DeroCreateNewWalletResult = res.value;
-  return value;
+  if (res.value) {
+    const value: DeroCreateNewWalletResult = {
+      ...res.value,
+      json: convertFileDataToJson(res.value.fileData),
+    };
+    res.value = value;
+  }
+  return res;
 };
+
+// todo RecoverWalletFromDisk
+// WalletGetEncryptedData
+// WalletGetTopoHeight
+// WalletGetAddress
+
+export interface DeroWasmWalletGetBalanceI {
+  walletKey: string;
+  scid: string;
+}
+
+export const DeroWasmWalletGetBalance = async (
+  params: DeroWasmWalletGetBalanceI
+) => {
+  // the result is stored in the window interface with this key
+  const asyncKey = 'DeroWasmWalletGetBalance';
+  const res = DeroWasmWindow.WalletGetBalance(
+    asyncKey,
+    params.walletKey,
+    params.scid
+  );
+  return res;
+};
+
+// todo
+// WalletGetSeed
+// WalletGetHexSeed
+// WalletIsRegistered
+
+// todo wallet transfers
+// WalletTransfer
+// WalletGetTransfers
+// WalletSendTransaction
+
+// todo wallet register the old slow way
+
+export const FastRegister = async () => {
+  const asyncKey = 'FastRegister';
+  const res = DeroWasmWindow.FastRegister(asyncKey);
+
+  return res;
+};
+/*
+let fastRegResp = window.FastRegister(1)
+console.log("fastRegResp",fastRegResp)
+let key = `RegistrationStatus_<number: 1>`
+console.log("window",window[key])
+*/
 
 //
 // Daemon Node
@@ -92,27 +157,52 @@ export const DeroWasmInitialize = async (
   daemonEndpoint: string
 ) => {
   const res = DeroWasmWindow.Initialize(env, daemonEndpoint);
-
   return res;
 };
 
+export interface DeroWasmDecodeHexTransactionResponse
+  extends DeroWasmWalletResponse {
+  value: string;
+}
 export const DeroWasmDecodeHexTransaction = async (txHex: string) => {
-  const key = 'DaemonSetAddressResult';
-  const res = DeroWasmWindow.DecodeHexTransaction(txHex);
-  if (!res.value) return;
-  // todo need type for this
+  const res: DeroWasmDecodeHexTransactionResponse =
+    DeroWasmWindow.DecodeHexTransaction(txHex);
   return res;
 };
+
+export interface DeroWasmVerifyAddressResponse extends DeroWasmWalletResponse {
+  value: boolean;
+}
+export const DeroWasmVerifyAddress = async (addrString: string) => {
+  const res: DeroWasmVerifyAddressResponse =
+    DeroWasmWindow.VerifyAddress(addrString);
+  return res;
+};
+
+export interface DeroWasmCheckSeedResponse extends DeroWasmWalletResponse {
+  value: boolean;
+}
+export const DeroWasmCheckSeed = async (addrString: string) => {
+  const res = DeroWasmWindow.CheckSeed(addrString);
+  // for some reason if the checkseed
+  // fails the go wasm wrapper returns an error in err and doesnt return a bool for value
+  // and if it succeeds it returns nil, nil.
+  return res;
+};
+
 export const DeroWasmDaemonSetAddress = async (addr: string) => {
   const asyncKey = 'DaemonSetAddressResult';
   const res = DeroWasmWindow.DaemonSetAddress(asyncKey, addr);
   return res;
 };
 
+export interface DeroWasmDaemonGetTopoHeightResponse
+  extends DeroWasmWalletResponse {
+  value: string;
+}
 export const DeroWasmDaemonGetTopoHeight = async () => {
-  //@ts-ignore
-  const res = window.DaemonGetTopoHeight();
-  //console.log("get height", res)
+  const res: DeroWasmDaemonGetTopoHeightResponse =
+    DeroWasmWindow.DaemonGetTopoHeight();
   return res;
 };
 
@@ -121,8 +211,6 @@ export interface DeroWasmDaemonCallParams {
   method: string;
   data: string;
 }
-// many of the dero wasm calls are async
-// you must provide a key to find the returned data in the window interface
 export const DeroWasmDaemonCall = async (params: DeroWasmDaemonCallParams) => {
   const res = DeroWasmWindow.DaemonCall(
     params.asyncKey,
@@ -133,49 +221,39 @@ export const DeroWasmDaemonCall = async (params: DeroWasmDaemonCallParams) => {
   return res;
 };
 
+// many of the dero wasm calls are async
+// you must provide a key to find the returned data in the window interface
 export const GetBlock = async () => {
   const method = 'DERO.GetBlock';
   const json = {};
-  const res = await DeroWasmDaemonCall({
-    method: method,
-    asyncKey: method,
-    data: JSON.stringify(json),
-  });
+  const res = DeroWasmWindow.DaemonCall(method, method, JSON.stringify(json));
   console.log('get block', res);
   return res;
 };
 
-export interface DeroWasmOpenWalletI {
-  walletKey: string;
-  password: string;
-  fileData: any; // Uint8Array
-}
+//
+// Utils
+//
 
-export const DeroWasmOpenWallet = async (params: DeroWasmOpenWalletI) => {
-  console.log('openwallet params', params);
-  const res = DeroWasmWindow.OpenWallet(
-    params.walletKey,
-    params.password,
-    params.fileData,
-    true
+export function hex_to_ascii(str1: string) {
+  const hex = str1.toString();
+  let str = '';
+  for (let n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
+export const toHexString = (bytes: any) => {
+  //@ts-ignore
+  return bytes.reduce(
+    (str: string, byte: any) => str + byte.toString(16).padStart(2, '0'),
+    ''
   );
-  console.log('openwallet ', res);
 };
-
-export interface DeroWasmWalletGetBalanceI {
-  walletKey: string;
-  scid: string;
-}
-
-export const DeroWasmWalletGetBalance = async (
-  params: DeroWasmWalletGetBalanceI
-) => {
-  const asyncKey = 'DeroWasmWalletGetBalance';
-  console.log('get balance params', params);
-  const res = DeroWasmWindow.WalletGetBalance(
-    asyncKey,
-    params.walletKey,
-    params.scid
-  );
-  console.log('balance ', res);
+const convertFileDataToJson = (fileData: ArrayBuffer) => {
+  const charArray = new Uint8Array(fileData);
+  const hexString = toHexString(charArray);
+  const decode = hex_to_ascii(hexString);
+  const json = JSON.parse(decode);
+  return json;
 };
